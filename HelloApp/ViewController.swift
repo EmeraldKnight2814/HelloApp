10f16add4a206c580d7a15b4f8d605d355d5fc53
Daniel J Crowley <61795248+TheMusicMan28@users.noreply.github.com>
2023-01-24 10:45:27 -0600

    Hello App written

--- HelloApp/ViewController.swift
+++ HelloApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var 名前: UITextField!
+    
+    
+    @IBOutlet weak var 画面のラベル_outlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func ボタン_action(_ sender: Any) {
+        // read input name and store it
+        var input = 名前.text!
+        
+        // perform stirng interpolation
+        画面のラベル_outlet.text = "Hello There, \(input)!"
+    }
+    
 
 }
 
